5460e3606d6eafe73a461d3a29a33a1399b4906f
Fanbayy <fandi.bayu110@gmail.com>
2021-06-25 17:20:35 +0700

    add 2 feature

---
+++ src/script/index.js
@@ -0,0 +1,56 @@
+// animated js
+
+var myVar;
+function myFunction() {
+  myVar = setTimeout(showPage, 100);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+// see more js
+function toggleTextMB() {
+  var points = document.getElementById("pointsMB");
+
+  var showMoreText = document.getElementById("moreTextMB");
+
+  var buttonText = document.getElementById("textButtonMB");
+
+  if (points.style.display === "none") {
+    showMoreText.style.display = "none";
+
+    points.style.display = "inline";
+
+    buttonText.innerHTML = "Show More";
+  } else {
+    showMoreText.style.display = "inline";
+
+    points.style.display = "none";
+
+    buttonText.innerHTML = "Show Less";
+  }
+}
+
+function toggleTextDK() {
+  var points = document.getElementById("pointsDK");
+
+  var showMoreText = document.getElementById("moreTextDK");
+
+  var buttonText = document.getElementById("textButtonDK");
+
+  if (points.style.display === "none") {
+    showMoreText.style.display = "none";
+
+    points.style.display = "inline";
+
+    buttonText.innerHTML = "Show More";
+  } else {
+    showMoreText.style.display = "inline";
+
+    points.style.display = "none";
+
+    buttonText.innerHTML = "Show Less";
+  }
+}
